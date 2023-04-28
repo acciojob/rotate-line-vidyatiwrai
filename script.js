fd11654c389757843ade391f14ac39053d7f1214
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-28 13:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+var angle = 0;
+      setInterval(function() {
+        angle += 2;
+        document.getElementById("line").style.transform = "rotate(" + angle + "deg)";
+      }, 20);
